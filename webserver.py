3de6ce3c8df76cad30e16c29c3c1fae041676f47
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-11-22 00:52:28 +0100

    Implemented key to car control mapping

--- webserver.py
+++ webserver.py
@@ -1,18 +1,35 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from car import Car
+pican = Car()
+
 @app.route('/')
 def root():
     return render_template('pican_main.html')
 
 @app.route('/keydown/<key>', methods=['POST'])
 def keydown(key):
-    print('DATA: {}'.format(key))
+    if key == 'a':
+        pican.left()
+    else if key == 'd':
+        pican.right()
+    else if key == 'w':
+        pican.forward()
+    else if key == 's':
+        pican.backward()
     return ('', 204)
 
 @app.route('/keyup/<key>', methods=['POST'])
 def keyup(key):
-    print('DATA: {}'.format(key))
+    if key == 'a':
+        pican.straight()
+    else if key == 'd':
+        pican.straight()
+    else if key == 'w':
+        pican.stop()
+    else if key == 's':
+        pican.stop()
     return ('', 204)
 
 if __name__ == '__main__':
