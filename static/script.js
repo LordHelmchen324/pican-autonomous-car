d5556284c8e6ce5b88480a7024de2d040db54958
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-11-22 00:36:55 +0100

    Implemented key up and down transmition

--- static/script.js
+++ static/script.js
@@ -1,17 +1,14 @@
 document.addEventListener('keydown', function(e) {
-    if (e.key == 'w') {
-        document.getElementById('keytext').innerHTML = 'Up'
-    } else if (e.key == 's') {
-        document.getElementById('keytext').innerHTML = 'Down'
-    } else if (e.key == 'a') {
-        document.getElementById('keytext').innerHTML = 'Left'
-    } else if (e.key == 'd') {
-        document.getElementById('keytext').innerHTML = 'Right'
-    }
+    if (e.key == 'w') $('#keytext').text('Up')
+    else if (e.key == 's') $('#keytext').text('Down')
+    else if (e.key == 'a') $('#keytext').text('Left')
+    else if (e.key == 'd') $('#keytext').text('Right')
+
+    if (['w', 'a', 's', 'd'].includes(e.key)) $.post('keydown/' + e.key)
 })
 
 document.addEventListener('keyup', function(e) {
-    if (['w', 'a', 's', 'd'].includes(e.key)) {
-        document.getElementById('keytext').innerHTML = 'None'
-    }
+    if (['w', 'a', 's', 'd'].includes(e.key)) $('#keytext').text('None')
+
+    if (['w', 'a', 's', 'd'].includes(e.key)) $.post('keyup/' + e.key)
 })
--- webserver.py
+++ webserver.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def root():
     return render_template('pican_main.html')
+
+@app.route('/keydown/<key>', methods=['POST'])
+def keydown(key):
+    print('DATA: {}'.format(key))
+    return ('', 204)
+
+@app.route('/keyup/<key>', methods=['POST'])
+def keyup(key):
+    print('DATA: {}'.format(key))
+    return ('', 204)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
