5ee7d23bbd3470af95587af79859a1c4a4c3b2ce
Jan Kaiser <jan.kaiser.email@googlemail.com>
2019-11-21 23:54:21 +0100

    Implemented resuable Car class and moved test code

--- car.py
+++ car.py
@@ -1,60 +1,39 @@
 import RPi.GPIO as GPIO
-from time import sleep
 
-lftPin, rgtPin, fwdPin, bwdPin = 31, 33, 35, 37
+class Car:
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(lftPin, GPIO.OUT)
-GPIO.setup(rgtPin, GPIO.OUT)
-GPIO.setup(fwdPin, GPIO.OUT)
-GPIO.setup(bwdPin, GPIO.OUT)
+    PIN_LEFT, PIN_RIGHT, PIN_FORWARD, PIN_BACKWARD = 31, 33, 35, 37
 
-def stop():
-    GPIO.output(fwdPin, GPIO.LOW)
-    GPIO.output(bwdPin, GPIO.LOW)
+    def __init__():
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PIN_LEFT, GPIO.OUT)
+        GPIO.setup(PIN_RIGHT, GPIO.OUT)
+        GPIO.setup(PIN_FORWARD, GPIO.OUT)
+        GPIO.setup(PIN_BACKWARD, GPIO.OUT)
 
-def goForwards():
-    GPIO.output(fwdPin, GPIO.HIGH)
-    GPIO.output(bwdPin, GPIO.LOW)
+    def __del__():
+        GPIO.cleanup()
 
-def goBackwards():
-    GPIO.output(fwdPin, GPIO.LOW)
-    GPIO.output(bwdPin, GPIO.HIGH)
+    def stop():
+        GPIO.output(fwdPin, GPIO.LOW)
+        GPIO.output(bwdPin, GPIO.LOW)
 
-def steerStraight():
-    GPIO.output(lftPin, GPIO.LOW)
-    GPIO.output(rgtPin, GPIO.LOW)
+    def forwards():
+        GPIO.output(fwdPin, GPIO.HIGH)
+        GPIO.output(bwdPin, GPIO.LOW)
 
-def steerLeft():
-    GPIO.output(lftPin, GPIO.HIGH)
-    GPIO.output(rgtPin, GPIO.LOW)
+    def backwards():
+        GPIO.output(fwdPin, GPIO.LOW)
+        GPIO.output(bwdPin, GPIO.HIGH)
 
-def steerRight():
-    GPIO.output(lftPin, GPIO.LOW)
-    GPIO.output(rgtPin, GPIO.HIGH)
+    def straight():
+        GPIO.output(lftPin, GPIO.LOW)
+        GPIO.output(rgtPin, GPIO.LOW)
 
-# ------------------------
+    def left():
+        GPIO.output(lftPin, GPIO.HIGH)
+        GPIO.output(rgtPin, GPIO.LOW)
 
-stop()
-steerStraight()
-
-sleep(3)
-
-goForwards()
-sleep(1.5)
-stop()
-sleep(0.5)
-steerLeft()
-goBackwards()
-sleep(1.5)
-stop()
-sleep(0.5)
-steerRight()
-goForwards()
-sleep(1.5)
-stop()
-steerStraight()
-
-# ------------------------
-
-GPIO.cleanup()
+    def right():
+        GPIO.output(lftPin, GPIO.LOW)
+        GPIO.output(rgtPin, GPIO.HIGH)
